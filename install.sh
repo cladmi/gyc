806b51c23c9024bd27892a99629b31d66831cb16
Gaëtan Harter <hartergaetan@gmail.com>
2012-12-28 23:08:34 +0100

    Add comments to explain the script

--- install.sh
+++ install.sh
@@ -13,7 +13,10 @@ alias gyc="git --git-dir=${GYC_GIT_DIR} --work-tree=${GYC_WORK_TREE}"
 GYC="git --git-dir=${GYC_GIT_DIR} --work-tree=${GYC_WORK_TREE}"
 
 
-function create_gyc_repository() {
+#
+# Create or update repository in $GYC_GIT_DIR
+#
+__create_gyc_repository() {
     echo "Create gyc repository with
         root_dir = '${GYC_WORK_TREE}'
         git_dir  = '${GYC_GIT_DIR}'"
@@ -28,8 +31,16 @@ function create_gyc_repository() {
 
 }
 
-function __setup_gyc_repository() {
+#
+# Configure repository,
+#    can be re-run if no modifications have been hand made to the repository
+#
+__setup_gyc_repository() {
 
+    # init/reinit repository
+    $GYC init > /dev/null
+
+    # Exclude all files by default
     EXCL=${GYC_GIT_DIR}/info/exclude
     grep -q '^\*$' $EXCL 2> /dev/null # only * in line
     if [ $? -ne 0 ]; then
@@ -37,22 +48,23 @@ function __setup_gyc_repository() {
         echo '*' >> $EXCL
     fi
 
-    $GYC init > /dev/null
-
+    # Use hostname as branch name
     $GYC branch | grep -q " ${HOSTNAME}$" 2> /dev/null # test branch exist
     if [ $? -ne 0 ]; then
         $GYC checkout -b ${HOSTNAME} 2>/dev/null
     fi
 
-    # alias to add files as by default all files are excluded
+    # alias to add files
+    #   by default all files are excluded so -f is required to add
     $GYC config alias.fadd 'add -f'
 
+    # Hooks to match files
     cp -r ${SCRIPT_FOLDER}/hooks ${GYC_GIT_DIR}/
 
 }
 
 
-create_gyc_repository
+__create_gyc_repository
 
 echo
 echo 'Add the following alias to your .bashrc/.zshrc/.yourrc file and source it to add alias'
