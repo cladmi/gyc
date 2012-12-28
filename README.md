42349ad8fc73d1a3b1057bc5b4efb0e125156268
Gaëtan Harter <hartergaetan@gmail.com>
2012-12-28 09:51:05 +0100

    Moving to an install file instead of a to source file Add hooks to match password file

--- README.md
+++ README.md
@@ -4,12 +4,23 @@ Gyc
 Wrapper around `git` to manage system configuration files in a repository.
 
 
+Install
+-------
+
+Setup the following variable in the `install.sh` file if you want to change them
+
+    GYC_WORK_TREE='/'
+    GYC_GIT_DIR="${HOME}/.gyc/git_bare"
+
+Run `install.sh` and add the `alias` line to your .rc file
+
+
 Features
 --------
 
 * creation command                         -> In progress
 * share repository between all computers   -> branches named as hostname
-* try to detect "password" stored in files -> Todo
+* try to detect "password" stored in files -> Matching some patterns, see in hooks
 
 * write permissions when pulling ?
 * when to pull for news ?
--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
+#! /bin/bash
+#
 # git@github.com:cladmi/gyc.git
+#
+
+# change to the install folder
+SCRIPT_FOLDER="$(dirname $0)"
 
 GYC_WORK_TREE='/'
 GYC_GIT_DIR="${HOME}/.gyc/git_bare"
 
-TEST=true
-
 alias gyc="git --git-dir=${GYC_GIT_DIR} --work-tree=${GYC_WORK_TREE}"
+GYC="git --git-dir=${GYC_GIT_DIR} --work-tree=${GYC_WORK_TREE}"
+
 
 function create_gyc_repository() {
     echo "Create gyc repository with
@@ -15,8 +21,7 @@ function create_gyc_repository() {
     mkdir -p $(dirname ${GYC_GIT_DIR})
 
     if [ -e ${GYC_GIT_DIR} ]; then
-        echo "${GYC_GIT_DIR} allready exist"
-        ${TEST} || exit 0
+        echo "Updating existing repository '${GYC_GIT_DIR}"
     fi
 
     __setup_gyc_repository
@@ -26,22 +31,31 @@ function create_gyc_repository() {
 function __setup_gyc_repository() {
 
     EXCL=${GYC_GIT_DIR}/info/exclude
-    grep -q '^\*$' $EXCL 2> /dev/null
+    grep -q '^\*$' $EXCL 2> /dev/null # only * in line
     if [ $? -ne 0 ]; then
-        echo "pattern not found"
         echo '# Exclude all files by default, to add files, use "-f"' >> $EXCL
         echo '*' >> $EXCL
-    else
-        echo "pattern found"
     fi
 
+    $GYC init > /dev/null
 
-    gyc init
-    gyc checkout -b ${HOSTNAME}
+    $GYC branch | grep -q " ${HOSTNAME}$" 2> /dev/null # test branch exist
+    if [ $? -ne 0 ]; then
+        $GYC checkout -b ${HOSTNAME} 2>/dev/null
+    fi
 
     # alias to add files as by default all files are excluded
-    gyc config alias.fadd 'add -f'
+    $GYC config alias.fadd 'add -f'
 
-
+    cp -r ${SCRIPT_FOLDER}/hooks ${GYC_GIT_DIR}/
 
 }
+
+
+create_gyc_repository
+
+echo
+echo 'Add the following alias to your .bashrc/.zshrc/.yourrc file and source it to add alias'
+echo
+echo $(alias gyc)
+
